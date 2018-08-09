92b266f17495173caf402aa1053a49c84212216c
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-08-09 18:25:04 +0200

    add webscraping module

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-print("webscraping quotes...") # http://quotes.toscrape.com/
+from scrape_quotes import get_quotes_list
+
+print("webscraping quotes...")  # http://quotes.toscrape.com/
+quote_list = get_quotes_list()
 
 while True:
     print("playng game...")
+    print(quote_list[0])
     while True:
         replay = input("Do you want to play again? (y/n): ")
         if replay in ("Y", "y", "N", "n"):
             break
     if replay not in ("Y", "y"):
         break
-
---
+++ scrape_quotes.py
@@ -0,0 +1,31 @@
+import requests
+from quote import Quote
+from bs4 import BeautifulSoup
+
+
+def get_quotes_list():
+    page = 1
+    base_url = "http://quotes.toscrape.com"
+    all_quotes = []
+    while True:
+        url = base_url + f"/page/{page}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        quotes = soup.select(".text")
+        for quote in quotes:
+            text = quote.get_text()
+            author = quote.find_next("small").get_text()
+            details_url = quote.find_next("a")['href']
+            response_details = requests.get(base_url + details_url)
+            soup_details = BeautifulSoup(response_details.text, "html.parser")
+            born_date = soup_details.select(".author-born-date")[0].get_text()
+            born_location = soup_details.select(
+                ".author-born-location")[0].get_text()
+            birth = born_date + " " + born_location
+            bio = soup_details.select(".author-description")[0].get_text()
+            all_quotes.append(Quote(text, author, birth, bio))
+        if not soup.select(".next"):
+            break
+        page += 1
+
+    return all_quotes
