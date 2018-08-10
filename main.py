0e85b06664e2cb9469129a715740a63fed633fb6
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-08-10 13:59:56 +0200

    add game logic

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from scrape_quotes import get_quotes_list
+from play_game import play
+from quote import Quote
 
 print("webscraping quotes...")  # http://quotes.toscrape.com/
 quote_list = get_quotes_list()
 
 while True:
-    print("playng game...")
-    print(quote_list[0])
+    play(quote_list)
     while True:
         replay = input("Do you want to play again? (y/n): ")
         if replay in ("Y", "y", "N", "n"):
---
+++ play_game.py
@@ -0,0 +1,32 @@
+from random import choice
+
+
+def play(quote_list):
+    guesses = 3
+    found = False
+    selected_quote = choice(quote_list)
+    print("Here is a Quote:")
+    print("\n")
+    print(selected_quote.quote)
+    print("\n")
+    while guesses >= 0 and not found:
+        print_hint(guesses, selected_quote)
+        answer = input(
+            f"Who said this? ({guesses} guesses remaining): ")
+        if answer.lower() == selected_quote.author.lower():
+            print("You guessed correctly! Congratulations!")
+            found = True
+        else:
+            print("Sorry wrong answer!")
+            guesses -= 1
+    if not found:
+        print(f"The author was: {selected_quote.author}! Better luck next time!")
+
+def print_hint(guesses, selected_quote):
+    prefix = "Here's a hint: The author "
+    if guesses == 2:
+        print(prefix + f"was born in {selected_quote.birth}")
+    if guesses == 1:
+        print(prefix + f"initials are {selected_quote.get_initials()}")
+    if guesses == 0:
+        print(prefix + f"bio is:\n\n{selected_quote.bio_without_author_name()}")
