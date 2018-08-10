65532c00b70b926d355efbad2deefb592f6acbbb
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-08-10 15:49:36 +0200

    add saving list of quotes to file for future execution of the game

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from scrape_quotes import get_quotes_list
 from play_game import play
 from quote import Quote
+from os.path import exists
+from pathlib import Path
+import pickle
 
-print("webscraping quotes...")  # http://quotes.toscrape.com/
-quote_list = get_quotes_list()
+file_is_valid = False
+file = Path("quotes.pickle")
+if exists(file):
+    with open(file, "rb") as file:
+        quote_list = pickle.load(file)
+        if len(quote_list) > 0:
+            print("file is valid, loading quotes...")
+            file_is_valid = True
+        else:
+            print("file is not valid")
+else:
+    print("no quotes file found")
+if not file_is_valid:
+    print("webscraping quotes...")  # http://quotes.toscrape.com/
+    quote_list = get_quotes_list()
+    with open(file, "wb") as file:
+        pickle.dump(quote_list, file)
 
 while True:
     play(quote_list)
