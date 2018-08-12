0b26366ae0506886b952618fe0c3e8450c032c7d
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2018-08-12 11:08:53 +0200

    minor changes to main file

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ while True:
         replay = input("Do you want to play again? (y/n): ")
         if replay in ("Y", "y", "N", "n"):
             break
-    if replay not in ("Y", "y"):
+    if replay in ("N", "n"):
         break
